4575329431c66d1e1448fb457af5cfcd34113637
Matthew McEneaney <matthew.mceneaney@duke.edu>
2023-11-13 14:47:54 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# CLAS12 RGH SIDIS Simulation Studies
+Basic repository for RGH simulation work.
+
+#
+
+Contact: matthew.mceneaney@duke.edu
---
+++ job.sh
@@ -0,0 +1,33 @@
+#!/bin/bash                                                  
+
+# Set job info
+export MCINDEX=0 #NOTE: NEED TO CHANGE FROM SETUP.SH
+export BASENAME="out_rgh_NH3_${MCINDEX}"
+export BASEDIR="/work/clas12/users/$USER/rgh_simulation"
+export BEAM_ENERGY=10.6
+export TARGET_TYPE="proton"
+export NEVENTS=100 #NOTE: NEED TO CHANGE FROM SETUP.SH? -> Definitely increase for actual jobs but keep this for testing...
+export EVPFILE=$NEVENTS
+export GCARD="$BASEDIR/rgh_new.gcard"
+export YAML="$BASEDIR/rgh_new.yaml"
+export OUTDIR="/volatile/clas12/users/$USER/rgh_simulation"
+mkdir -p $OUTDIR
+
+# Run MC event Simulations (nh3 target 10.6 GeV)
+export OUTDIR_LUND="${OUTDIR}/lund"
+mkdir -p $OUTDIR_LUND
+export LUNDFILE=$OUTDIR_LUND/$BASENAME
+export LUNDFILE=`clasdis --beam $BEAM_ENERGY --targ $TARGET_TYPE --trig $NEVENTS --nmax $EVPFILE --path $LUNDFILE  | grep '^Data file Name .*' | grep -Eo $BASENAME.*`
+export LUNDFILE=$OUTDIR_LUND/$LUNDFILE
+
+# Run GEMC detector simulation
+export OUTDIR_GEMC="${OUTDIR}/gemc"
+mkdir -p $OUTDIR_GEMC
+export GEMCFILE=$OUTDIR_GEMC/$BASENAME.hipo
+gemc $GCARD -USE_GUI=0 -RUNNO=11 -INPUT_GEN_FILE="LUND, $LUNDFILE" -OUTPUT="hipo, $GEMCFILE" -N=$NEVENTS
+
+# Run CLAS12 reconstruction
+export OUTDIR_REC="${OUTDIR}/cooked"
+mkdir -p $OUTDIR_REC
+export RECFILE=$OUTDIR_REC/$BASENAME.hipo
+recon-util -i $GEMCFILE -o $RECFILE -n $NEVENTS -y $YAML
