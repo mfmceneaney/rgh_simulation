7ce0c8f324f89871ceb8092a86b4cf4ecae4f465
Matthew McEneaney <matthew.mceneaney@duke.edu>
2024-11-12 14:53:45 -0500

    Updated `job.sh` to add additional parameters for clasdis and gemc and also to not truncate the clasdis file name.

--- job.sh
+++ job.sh
@@ -15,6 +15,8 @@ export EVPFILE=$NEVENTS
 export GCARD="$BASEDIR/rgh_physics.gcard"
 export YAML="$BASEDIR/rgh_physics.yaml"
 export OUTDIR="/volatile/clas12/users/$USER/rgh_simulation"
+export XMIN=0.05 #NOTE: THESE ARE KINEMATIC LIMITS FOR CLASDIS GENERATOR.
+export XMAX=0.95
 mkdir -p $OUTDIR
 
 function check_task_status() {
@@ -41,9 +43,11 @@ function check_task_status() {
 # Run MC event Simulations (nh3 target 10.6 GeV)
 export OUTDIR_LUND="${OUTDIR}/lund"
 mkdir -p $OUTDIR_LUND
-export LUNDFILE=$OUTDIR_LUND/$BASENAME #NOTE: This cannot be too long, otherwise clasdis will truncate it.
-clasdis --beam $BEAM_ENERGY --targ $TARGET_TYPE --trig $NEVENTS --nmax $EVPFILE --path $LUNDFILE
+cd $OUTDIR_LUND #NOTE: Since clasdis does not like long input path names and just truncates them just cd to here and use basename
+export LUNDFILE=$BASENAME #NOTE: This cannot be too long, otherwise clasdis will truncate it.
+clasdis --beam $BEAM_ENERGY --targ $TARGET_TYPE --trig $NEVENTS --nmax $EVPFILE --path $LUNDFILE --x $XMIN $XMAX
 export LUND_TASK_STATUS=$?
+cd - #NOTE: cd back to wherever you were before clasdis
 export LUNDFILE=`ls $OUTDIR_LUND/${BASENAME}clasdis*.dat`
 check_task_status "clasdis" $LUNDFILE $LUND_TASK_STATUS 1
 
@@ -51,7 +55,7 @@ check_task_status "clasdis" $LUNDFILE $LUND_TASK_STATUS 1
 export OUTDIR_GEMC="${OUTDIR}/gemc"
 mkdir -p $OUTDIR_GEMC
 export GEMCFILE=$OUTDIR_GEMC/$BASENAME.hipo
-gemc $GCARD -USE_GUI=0 -RUNNO=11 -INPUT_GEN_FILE="LUND, $LUNDFILE" -OUTPUT="hipo, $GEMCFILE" -N=$NEVENTS
+gemc $GCARD -SAVE_ALL_MOTHERS=1 -SKIPREJECTEDHITS=1 -NGENP=50 -INTEGRATEDRAW="*" -USE_GUI=0 -RUNNO=11 -INPUT_GEN_FILE="LUND, $LUNDFILE" -OUTPUT="hipo, $GEMCFILE" -N=$NEVENTS
 export GEMC_TASK_STATUS=$?
 check_task_status "gemc" $GEMCFILE $GEMC_TASK_STATUS 2
 
@@ -73,6 +77,6 @@ export DST_TASK_STATUS=$?
 check_task_status "hipo-utils -filter" $DSTFILE $DST_TASK_STATUS 4
 
 # Clean up intermediate files
-rm $LUNDFILE
+#rm $LUNDFILE
 rm $GEMCFILE
 rm $RECFILE
