618a4364d1c10f9c409b4312fd64aa3504b70134
Matthew McEneaney <matthew.mceneaney@duke.edu>
2024-11-12 14:52:12 -0500

    Updated SLURM parameters in `submit.sh`.

--- submit.sh
+++ submit.sh
@@ -3,13 +3,12 @@
 #SBATCH --account=clas12
 #SBATCH --partition=production
 #SBATCH --job-name=rgh_sidis_NH3
-#SBATCH --constraint=general
 #SBATCH --mail-user=%u@jlab.org
 #SBATCH --output=/farm_out/%u/%x-%j-%N.out
 #SBATCH --error=/farm_out/%u/%x-%j-%N.err
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --gres=disk:5000
-#SBATCH --ntasks=1
+#SBATCH -c 1
 #SBATCH --mem-per-cpu=2000
 
 /work/clas12/users/$USER/rgh_simulation/job.sh
