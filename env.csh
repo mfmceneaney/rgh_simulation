1a7c73f45c5efb077b141c3ef3cbfe31d2a81402
Matthew McEneaney <matthew.mceneaney@duke.edu>
2025-04-15 14:43:55 -0400

    feat: Added environment scripts.

---
+++ env.csh
@@ -0,0 +1,5 @@
+#!/bin/tcsh
+
+setenv RGH_SIM_HOME $PWD
+setenv RGH_SIM_WORK_DIR "/work/clas12/users/$USER/rgh_simulation"
+setenv RGH_SIM_VOL_DIR "/volatile/clas12/users/$USER/rgh_simulation"
---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export RGH_SIM_HOME=$PWD
+export RGH_SIM_WORK_DIR="/work/clas12/users/$USER/rgh_simulation"
+export RGH_SIM_VOL_DIR="/volatile/clas12/users/$USER/rgh_simulation"
