bcbf6aff90fe9b61658d193be7fe9453195c57ab
Matthew F. McEneaney <mfmceneaney@gmail.com>
2025-11-12 15:35:03 -0500

    fix: Fix paths in setup script.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd $RGH_SIM_WORK_DIR
+cd $RGH_SIM_HOME
 
 # Replace local paths in gcard using pwd
-sed -i.bak "s;/work/clas12/mccontalb/GEMC/gcard;$RGH_SIM_WORK_DIR/mccontalb_GEMC_gcard;g" *.gcard
+sed -i.bak "s;/work/clas12/mccontalb/GEMC/gcard;$RGH_SIM_HOME/mccontalb_GEMC_gcard;g" *.gcard
